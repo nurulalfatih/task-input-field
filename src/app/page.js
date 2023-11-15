5c0791da354218787296b1c795c0b8e96a756d39
nurulalfatih <bwsthetic@gmail.com>
2023-11-15 17:14:17 +0800

    membuat input field

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,61 @@
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
-import styles from './page.module.css'
+import './globals.css'
 
 export default function Home() {
+
+  const [input, setInput] = useState('')
+  const [nama, setNama] = useState('Nurul Alfatih Khumaera Azzahra H')
+
+
+  const handlerGantiNama = () => {
+    setNama(input);
+  }
+
+  const handlerInput = (val) => {
+    setInput(val);
+  }
+
+  function enterButton(e) {
+    if (e.code == "Enter") handlerGantiNama();
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <div className='body'>
+      <div className="banner-container">
+        <div className="header-banner-wrapper">
+          <div className="foto">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+            src="/assets/pasphoto.png"
+            fill
+            objectFit='contain'
             />
-          </a>
+          </div>
+          <div className="name">
+            <h1>{nama}</h1>
+            <div className="bio">
+              <p>D121211008</p>
+              <p>Teknik Informatika</p>
+            </div>
+          </div>
+        </div>
+        <div className="cta">
+          <input
+            placeholder='Add Your Name'
+            onInput={(val) => handlerInput(val.target.value)}
+            onKeyDown={(value) => {
+              enterButton(value)
+            }}
+          ></input>
+          <button onClick={() => {
+              handlerGantiNama();
+            }}>
+            <p>Change Name</p>
+          </button>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
